343fd3b064dad4ccc9943b52405e4f929098f59d
sakitakayanagi <melomelo.mymelody.sanrio@gmail.com>
2022-05-20 02:14:32 +0900

    issue4

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,15 +8,16 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
+    
     Plan.create(plan_params)
     redirect_to action: :index
-  end
-
-  private
-
-  def plan_params
-    params.require(:calendars).permit(:date, :plan)
-  end
+    end
+    
+    private
+    
+    def plan_params
+    params.require(:plan).permit(:date, :plan)
+    end
 
   def getWeek
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
